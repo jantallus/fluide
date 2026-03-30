8b460ea85dfd67661f5ff8ca8313a66347d45a27
jantallus <jantallus74@gmail.com>
2026-03-30 10:39:54 +0200

    code promo

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ app.post('/api/login', async (req, res) => {
     const token = jwt.sign(
       { id: user.id, email: user.email, role: user.role },
       JWT_SECRET,
-      { expiresIn: '24h' }
+      { expiresIn: '30m' }
     );
 
     res.json({
