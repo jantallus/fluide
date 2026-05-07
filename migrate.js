8a57529e2a2f2bd5ce3a704ec3ae5a79e235bcb6
jantallus <97134165+jantallus@users.noreply.github.com>
2026-05-07 18:38:12 +0200

    h

--- migrate.js
+++ migrate.js
@@ -118,6 +118,10 @@ const migrations = [
     name: '015_flight_types_activity_children',
     sql: `ALTER TABLE flight_types ADD COLUMN IF NOT EXISTS activity_children BOOLEAN NOT NULL DEFAULT false;`
   },
+  {
+    name: '016_flight_types_activity_gopro',
+    sql: `ALTER TABLE flight_types ADD COLUMN IF NOT EXISTS activity_gopro BOOLEAN NOT NULL DEFAULT false;`
+  },
 ];
 
 async function runMigrations() {
